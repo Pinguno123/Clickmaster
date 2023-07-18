b47616daf2c94058894d3a2b3e6fdf17f785c326
Pinguno123 <douglasanchez.7b@gmail.com>
2023-07-17 22:32:53 -0600

    update

--- db.php
+++ db.php
@@ -9,6 +9,4 @@ $connection = new mysqli($server, $user, $pass, $database);
 if ($connection->connect_error) {
     die("Connection failed:" . $connection->connect_error);
 }
-
-$prueba = "Hola";
 ?>
